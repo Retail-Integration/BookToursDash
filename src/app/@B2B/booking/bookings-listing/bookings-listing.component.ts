21dac9d338d93926279ca229e20fc38a71127748
bradshaw10 <eoghan.bradshaw@gmail.com>
2020-12-15 01:36:00 +0000

    data

--- src/app/@B2B/booking/bookings-listing/bookings-listing.component.ts
+++ src/app/@B2B/booking/bookings-listing/bookings-listing.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { B2BBooking, Tour } from './../../models/B2BBookingModels';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'ngx-bookings-listing',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsListingComponent implements OnInit {
 
-  constructor() { }
+
+  tours =  Array<Tour>();
+
+
+  constructor() {
+
+    this.tours = [
+      {
+        EventTypeCode: "001",
+        EventCode: "001",
+        EventEventDesc: "General Admission",
+        AvailableCapacity: 100,
+        EventCapacity: 0,
+        TourDateFormatted: "1/01/21",
+        TourDate : new Date(2021, 1, 1),
+        IsAvailable: true,
+        AlternativeTimes : ["test"]
+      }
+    ]
+   }
 
   ngOnInit(): void {
+  //  this.populateTours();
   }
+
+  // populateTours(): Tour{
+  //   this.tours.EventTypeCode = "001",
+  //   this.tours.EventCode= "001",
+  //   this.tours.EventEventDesc= "General Admission",
+  //   this.tours.AvailableCapacity= 100,
+  //   this.tours.AvailableCapacity= 0,
+  //   this.tours.TourDateFormatted= "1/01/21",
+  //   this.tours.TourDate = new Date(2021, 1, 1),
+  //   this.tours.IsAvailable= true,
+  //   this.tours.AlternativeTimes = ["test"]
+
+
+  //         return this.tours;
+  //}
 
 }
